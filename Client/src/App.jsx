3847dab0a0041d6c62fc7d6de2b48e671abefdc4
takshpatel28 <takshpatel416@gmail.com>
2025-03-28 16:12:52 +0530

    update

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes} from 'react-router'
 import Navbar from './Components/Navbar'
-import SignupModel from './Components/SignupModel'
 import HomePage from './Pages/Home'
 import Wrapper from './Components/Wrapper'
 import ResetPassword from './Components/ResetPassword'
 import Login from './Pages/Login'
-import Home from './Pages/Home'
 import Dashboard from './Pages/Dashboard'
 import Register from './Pages/Register'
 import Questioning from './Pages/Questioning'
@@ -17,7 +15,6 @@ const App = () => {
   return (
     <BrowserRouter>
     <Navbar />
-    <SignupModel />
       <Routes>
         <Route path='/' element={<HomePage />}/>
         <Route path='/dashboard' element={
