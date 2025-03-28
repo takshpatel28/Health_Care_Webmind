016ac9efb046f70b23c9157f06de80018645d246
Sujal Jagdev <148867954+Sujal-jagdev@users.noreply.github.com>
2025-03-28 06:04:51 +0000

    HOD Show Only Doctor Part Updated

--- Server/Controllers/role.Controller.js
+++ Server/Controllers/role.Controller.js
@@ -1,10 +1,10 @@
 const supabase = require("../Supabase/supabaseServer");
 
 module.exports.HOD = async (req, res) => {
-    const { specialization } = req.query; 
+    const { specialization } = req.query;
 
     try {
-        let query = supabase.from('doctors').select('*');
+        let query = supabase.from('doctors').select('*').eq('role','Doctor')
 
         if (specialization) {
             query = query.eq('specialization', specialization);
@@ -12,6 +12,7 @@ module.exports.HOD = async (req, res) => {
         const { data, error } = await query;
 
         if (error) {
+            console.log(error)
             return res.status(500).json({ error: "Database error", details: error.message });
         }
 
