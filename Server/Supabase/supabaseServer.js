47d923f2fa7d9b792de4065a3836aa40345ed9e9
Sujal Jagdev <148867954+Sujal-jagdev@users.noreply.github.com>
2025-03-29 11:29:22 +0000

    Updated

--- Server/Supabase/supabaseServer.js
+++ Server/Supabase/supabaseServer.js
@@ -4,7 +4,5 @@ require("dotenv").config()
 const Project_URL = process.env.PROJECT_URL;
 const Api_Key = process.env.API_KEY;
 
-const supabase = createClient(Project_URL,Api_Key,{ 
-    auth: { autoRefreshToken: false, persistSession: false } 
-});
+const supabase = createClient(Project_URL,Api_Key);
 module.exports = supabase;
