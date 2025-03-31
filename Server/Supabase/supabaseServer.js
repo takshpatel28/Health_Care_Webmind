b5163752e9ddbb2fc6c6e45e62d4eb8e821c9733
Sujal Jagdev <148867954+Sujal-jagdev@users.noreply.github.com>
2025-03-31 09:45:02 +0000

    Supabase Server Updated

--- Server/Supabase/supabaseServer.js
+++ Server/Supabase/supabaseServer.js
@@ -2,7 +2,7 @@ const { createClient } = require("@supabase/supabase-js");
 require("dotenv").config()
 
 const Project_URL = process.env.PROJECT_URL;
-const Api_Key = process.env.API_KEY;
+const Api_Key = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 const supabase = createClient(Project_URL,Api_Key);
 module.exports = supabase;
